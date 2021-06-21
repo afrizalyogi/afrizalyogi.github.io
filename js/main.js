f4b502f1d3fb59db15b047b722432f829b0359b3
Afrizal Yogi P <xxgdev@gmail.com>
2021-06-21 09:01:05 +0700

    Add 3D objects to website

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 function openNav() {
     if ( window.matchMedia("(max-width: 990px)").matches ) {
         document.getElementById("menu-items").style.width = "100%";
+        document.getElementById("menu-icon").style.opacity = "0%";
     }
 
     else {
-        document.getElementById("menu-items").style.width = "200px";
+        document.getElementById("menu-items").style.width = "250px";
+        document.getElementById("menu-icon").style.opacity = "0%";
     }
 }
 
 function closeNav() {
     document.getElementById("menu-items").style.width = "0";
+    document.getElementById("menu-icon").style.opacity = "100%";
 }
