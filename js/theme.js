feec3e9f3ada39681a13c2da11ea2e8f567f3d19
Afrizal Yogi <xxgdev@gmail.com>
2023-01-19 21:59:39 +0700

    Update Showcase

--- js/theme.js
+++ js/theme.js
@@ -1,25 +1,23 @@
-function toggleTheme () {
-    const htmlTag = document.getElementsByTagName('html')[0]
-    if (htmlTag.hasAttribute('data-theme')) {
-        htmlTag.removeAttribute('data-theme')
-        return window.localStorage.removeItem("site-theme")
-    }
+function toggleTheme() {
+	const htmlTag = document.getElementsByTagName("html")[0];
+	if (htmlTag.hasAttribute("data-theme")) {
+		htmlTag.removeAttribute("data-theme");
+		return window.localStorage.removeItem("site-theme");
+	}
 
-    htmlTag.setAttribute('data-theme', 'dark')
-    window.localStorage.setItem("site-theme", "dark")
+	htmlTag.setAttribute("data-theme", "dark");
+	window.localStorage.setItem("site-theme", "dark");
 }
 
-function applyInitialTheme () {
-    const theme = window.localStorage.getItem("site-theme")
-    if (theme !== null) {
-        const htmlTag = document.getElementsByTagName("html")[0]
-        htmlTag.setAttribute("data-theme", theme)
-        document.getElementById("input-toggle").checked = true;
-    }
+function applyInitialTheme() {
+	const theme = window.localStorage.getItem("site-theme");
+	if (theme !== null) {
+		const htmlTag = document.getElementsByTagName("html")[0];
+		htmlTag.setAttribute("data-theme", theme);
+		document.getElementById("input-toggle").checked = true;
+	}
 }
 
 applyInitialTheme();
 
-document
-    .getElementById("theme-toggle")
-    .addEventListener("click", toggleTheme);
+document.getElementById("theme-toggle").addEventListener("click", toggleTheme);
