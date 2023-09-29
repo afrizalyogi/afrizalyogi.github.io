7feb63c3edc687883bfddd4b1716a4def141d7f3
Afrizal Yogi <xxgdev@gmail.com>
2023-09-29 16:59:09 +0700

    Update Blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -8,6 +8,7 @@ import { faCalendar } from "@fortawesome/free-solid-svg-icons"
 
 export default async function Blog() {
 	const data = (await getData()) as Post[]
+
 	return (
 		<MainWrapper>
 			<section id="blog" className="py-20">
@@ -34,7 +35,7 @@ export default async function Blog() {
 					</div>
 				</div>
 				<div className="section-content">
-					<div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3">
+					<div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-6">
 						{data.map((item) => (
 							<Link href={`/blog/${item.slug.current}`}>
 								<div
